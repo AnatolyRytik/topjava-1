805f5ba1da14e5ce1db7f134e69af9f7f2d5626a
Anatoly <anatolyrytik@gmail.com>
2020-08-12 14:27:27 +0200

    Refactoring after review 1

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -21,8 +21,7 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Meal save(Meal meal, Integer currentUserId) {
-        repository.putIfAbsent(currentUserId, new ConcurrentHashMap<>());
-        Map<Integer, Meal> mealMap = repository.get(currentUserId);
+        Map<Integer, Meal> mealMap = repository.computeIfAbsent(currentUserId, k -> new ConcurrentHashMap<>());
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
             mealMap.put(meal.getId(), meal);
@@ -34,22 +33,24 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public boolean delete(int mealId, Integer currentUserId) {
-        return repository.get(currentUserId) != null && repository.get(currentUserId).remove(mealId) != null;
+        Map<Integer, Meal> mealMap = repository.get(currentUserId);
+        return mealMap != null && mealMap.remove(mealId) != null;
     }
 
     @Override
     public Meal get(int mealId, Integer currentUserId) {
-        return repository.get(currentUserId) != null ? repository.get(currentUserId).get(mealId) : null;
+        Map<Integer, Meal> mealMap = repository.get(currentUserId);
+        return mealMap != null ? mealMap.get(mealId) : null;
     }
 
     @Override
-    public List<Meal> getAll(Integer currentUserId) {
+    public List<Meal> getAll(Integer currentUserId) throws NullPointerException {
         if (currentUserId != null) {
             return repository.get(currentUserId).values()
                     .stream()
                     .sorted((m1, m2) -> m2.getDateTime().compareTo(m1.getDateTime()))
                     .collect(Collectors.toList());
-        } else return Collections.emptyList();
+        } else {return Collections.emptyList();}
     }
 }
 
